69162b20bedb09f17db864c1a1f4d25da781338e
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-05-07 18:18:58 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Content from './Content';
 
-function App() {
+
+
+
+
+
+class App extends Component {
+  state = {
+    content: 0,
+    addText: [null]
+  }
+
+
+  addMoreText = () => {
+    this.setState({
+      addText: [this.state.addText,'Another']
+    })
+  }
+
+  addContent = () => {
+    this.setState({
+      content: this.state.content +1
+    })
+
+  }
+
+  subtractContent = () => {
+    this.setState({
+      content: this.state.content -1
+    })
+  }
+
+  resetContent = () => {
+    this.setState({
+      content: 0
+    })
+  }
+  render() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick = {this.addContent}>Add</button>
+        <button onClick = {this.subtractContent}>Subtract</button>
+        <button onClick = {this.resetContent}>Reset</button>
+        <h1>{this.state.content}</h1>
+
+
+        <button onClick = {this.addMoreText}>Click for more</button>
+
+
       </header>
+
+      {this.state.addText.map((add =>
+
+      <ul key = 'add'>
+      <li>{add}</li>
+
+      </ul>
+      ))}
+
+
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Content.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Content extends Component {
+  render(){
+
+
+
+
+
+  return(
+    <div>
+    asdjsljhlasjlfasflk
+    </div>
+
+
+
+  )
+}
+}
+
+export default Content;
